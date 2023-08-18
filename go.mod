345b9e0fad14244f0e581f327847502900c44114
Adam Farmer <adam.farmer@safetyculture.io>
2023-08-18 12:31:56 +1000

    Added code

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/safety-adam/dominant-colours
+
+go 1.20
+
+require github.com/cenkalti/dominantcolor v1.0.1
+
+require (
+	github.com/disintegration/imaging v1.6.2 // indirect
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	_ "image/jpeg"
+	_ "image/png"
+	"os"
+
+	"github.com/cenkalti/dominantcolor"
+)
+
+func FindDominantColors(fileInput string) ([]color.RGBA, error) {
+	f, err := os.Open(fileInput)
+	if err != nil {
+		fmt.Println("File not found:", fileInput)
+		return []color.RGBA{}, err
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return []color.RGBA{}, err
+	}
+
+	colors := dominantcolor.FindN(img, 3)
+	return colors, nil
+}
+
+func main() {
+	colors, err := FindDominantColors("test2.jpeg")
+	if err != nil {
+		panic(err)
+	}
+
+	for n, c := range colors {
+		fmt.Printf("Color %d: %s https://www.color-hex.com/color/%s\n", n+1, dominantcolor.Hex(c), dominantcolor.Hex(c)[1:])
+	}
+}
